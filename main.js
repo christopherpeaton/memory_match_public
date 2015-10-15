c65b98253f94b16601ac624cca9459a53a359fa6
Christopher Eaton <christopherpeaton@gmail.com>
2015-10-15 16:23:50 -0700

    working on jquery - chris eaton

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 2;
+match_counter = 0;
+
+
+
+
+function card_clicked(card_container_element) {
+    console.log("card_container_element:",card_container_element);
+
+
+
+
+
+
+
+
+}
