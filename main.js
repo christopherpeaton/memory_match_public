04c49ee5ab0223b13471b714f0011c6591e6c8c2
Christopher Eaton <christopherpeaton@gmail.com>
2015-10-19 10:55:13 -0700

    workn on it

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 var attempts = 0;
 var games_played = 0;
@@ -22,30 +22,31 @@ function card_clicked(card) {
         first_card_clicked = img_src;
     } else {
         second_card_clicked = img_src;
-        attempts += 1;
         console.log("second card clicked!");
+        attempts += 1
+        console.log(attempts);
         if (first_card_clicked == second_card_clicked) {
             match_counter += 1;
             //console.log(match_counter);
-            //$(".card_has_been_selected").toggleClass('match');
+            $(".card_has_been_selected").toggleClass('match');
             console.log('a match!');
             first_card_clicked = null;
             second_card_clicked = null;
             if (match_counter == total_possible_matches) {
                 alert("You've Won!"); }
         } else {
-            $(".card_back").show("slow");
+            $(".card_has_been_selected").show("slow");
             first_card_clicked = null;
             second_card_clicked = null;
             console.log("they don't match");
 
-            //setTimeout( function () {
-            //   $(".match").find(".card").toggle()
-            //$("match").toggle().removeClass("match");
         }
 
     }
 
+    //function sidebar("#info") {
+    //    attempts += 1;
+    //}
 }
 
 
