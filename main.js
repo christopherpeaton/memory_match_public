3d8425136a650082e792c12e7e2b8a086bb04dbd
Christopher Eaton <christopherpeaton@gmail.com>
2015-10-19 18:20:45 -0700

    accuracy works, timer works, games played works -= chris eaton

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 9;
+var total_possible_matches = 2;
 var match_counter = 0;
 var attempts = 0;
 var games_played = 0;
@@ -12,11 +11,9 @@ function card_clicked(card) {
     //games_played += 1;
     //var card_front = $(img_src).find(".card_front img");
     //console.log("card:", card);
-
-    $(card).hide();
+    $(card).hide().addClass("card_has_been_selected");
     //console.log($(card_back).attr("src"));
     //var card_img = $(card_front).attr('src');
-
     //console.log('16');
     if (first_card_clicked == null) {
         console.log("first card clicked");
@@ -25,28 +22,37 @@ function card_clicked(card) {
         second_card_clicked = img_src;
         console.log("second card clicked!");
         attempts += 1;
-        update_stats();
         console.log(attempts);
         if (first_card_clicked === second_card_clicked) {
             match_counter += 1;
-            //console.log(match_counter);
+            console.log(match_counter);
+            $(".matches .label").text(match_counter);
             $(".card_has_been_selected").toggleClass('match');
             console.log('a match!');
             first_card_clicked = null;
             second_card_clicked = null;
+            console.log("total_possible_matches", total_possible_matches);
             if (match_counter === total_possible_matches) {
                 alert("You've Won!");
-                //games_played += 1;
+                games_played += 1;
+                $(".games-played .value").text(games_played);
+            }
         } else {
-            $(".card_has_been_selected").show("slow");
-            //first_card_clicked = null;
-            //second_card_clicked = null;
+            first_card_clicked = null;
+            second_card_clicked = null;
             console.log("they don't match");
+            waittime = setTimeout(function () {
+                $(".card_has_been_selected").show();
+            }, 2000);
         }
+        console.log("attempts", attempts);
+        howAccurate();
     }
 }
+function howAccurate() {
+    accuracy = match_counter / attempts;
+    $(".accuracy .label").text(accuracy);
 }
-
 
 function reset_clicked(reset) {
     $("#reset").find(".card_back").show("slow");
@@ -58,10 +64,10 @@ function reset_clicked(reset) {
     //$("#button_reset").click(function () {
     //    counter++;
     //    $(".games-played .value").text(games_played);
-    }
+}
 
 
-function update_stats () {
+function update_stats() {
     $(".attempts .value").text(attempts);
-    $(".games-played .value").text(games_played);
+
 }
