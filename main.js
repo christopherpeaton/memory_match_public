199960d8255ac63f2ddf8b5c866d3f6fc91660ef
Christopher Eaton <christopherpeaton@gmail.com>
2015-10-27 19:04:22 -0700

    all game area is created in dom, still needs work on functionality

--- main.js
+++ main.js
@@ -5,6 +5,24 @@ var match_counter = 0;
 var attempts = 0;
 var games_played = 0;
 
+$(document).ready(function () {
+    console.log("ready");
+    for (var i = 0; i < erics_photo_array.length; i++) {
+        console.log(erics_photo_array[i]);
+        var card_container = $("<div>", {
+            class: "card"
+        });
+        var card_front_results = card_front(erics_photo_array[i]);
+        $(card_container).append(card_front_results).append(card_back());
+        $("#game-area").append(card_container)
+    }
+    ;
+});
+var erics_photo_array = ["photos/2015-Porsche-918-Spyder.jpg", "photos/56-f100-truck-DSC-0037.jpg", "photos/ae86_levin_hatch_by_bramdc.jpg", "photos/e14592bd933e2191698240c13f74c791.jpg",
+    "photos/lowfall_0Vn6_0.jpg", "photos/Mercedes-Benz 190 Cosworth (2).jpg", "photos/morris-mini-1275-01.jpg", "photos/porsche-911-carrera-2.7-rs-04.jpg", "photos/web13.jpg", "photos/2015-Porsche-918-Spyder.jpg", "photos/56-f100-truck-DSC-0037.jpg", "photos/ae86_levin_hatch_by_bramdc.jpg", "photos/e14592bd933e2191698240c13f74c791.jpg",
+    "photos/lowfall_0Vn6_0.jpg", "photos/Mercedes-Benz 190 Cosworth (2).jpg", "photos/morris-mini-1275-01.jpg", "photos/porsche-911-carrera-2.7-rs-04.jpg", "photos/web13.jpg"
+];
+
 function card_back() {
     var card_back = $("<div>", {
         class: "card_back",
@@ -16,33 +34,23 @@ function card_back() {
         height: "200px",
         width: "150px"
     });
+    $(card_back).append(pic);
+    return card_back;
 }
 
-$('card_back').append(pic);
-$(".card").append(card_back)
-
-function card_front() {
+function card_front(frontcardpic) {
     var card_front = $("<div>", {
         class: "card_front"
     });
     var pic = $("<img>", {
-        src: "photos/2015-Porsche-918-Spyder.jpg",
+        src: frontcardpic,
         alt: "918",
         height: "200px",
         width: "150px"
     });
+    $(card_front).append(pic);
+    return card_front;
 }
-$("card_front").append(pic);
-$(".card").append(card_front);
-
-//<div class="card_front">
-//    <img src="photos/2015-Porsche-918-Spyder.jpg" alt="918" height="200px" width="150px">
-//</div>
-
-//<div class="card_back" onclick="card_clicked(this)">
-//    <img src="photos/Design-Paper-Vine-pattern-Art-Nouveau.jpg" alt="cardback" height="200px" ; width="150px" ;>
-//</div>
-
 
 function card_clicked(card) {
     var img_src = $(card).prev().find("img").attr("src");
