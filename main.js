0846b84acf2361e80ad9387eaa47025b6293cfe1
Christopher Eaton <christopherpeaton@gmail.com>
2015-12-03 11:21:00 -0800

    works well, doesnt reset perfectly after win

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ $(document).ready(function () {
         $(card_container).append(card_front_results).append(card_back());
         $("#game-area").append(card_container)
     }
-
 });
+
 var card_front_photo_array = ["photos/2015-Porsche-918-Spyder.jpg", "photos/56-f100-truck-DSC-0037.jpg", "photos/ae86_levin_hatch_by_bramdc.jpg", "photos/e14592bd933e2191698240c13f74c791.jpg",
     "photos/lowfall_0Vn6_0.jpg", "photos/Mercedes-Benz 190 Cosworth (2).jpg", "photos/morris-mini-1275-01.jpg", "photos/porsche-911-carrera-2.7-rs-04.jpg", "photos/web13.jpg", "photos/2015-Porsche-918-Spyder.jpg", "photos/56-f100-truck-DSC-0037.jpg", "photos/ae86_levin_hatch_by_bramdc.jpg", "photos/e14592bd933e2191698240c13f74c791.jpg",
     "photos/lowfall_0Vn6_0.jpg", "photos/Mercedes-Benz 190 Cosworth (2).jpg", "photos/morris-mini-1275-01.jpg", "photos/porsche-911-carrera-2.7-rs-04.jpg", "photos/web13.jpg"];
@@ -56,18 +56,17 @@ function card_front(frontcardpic) {
 
 function card_clicked(card) {
     var img_src = $(card).prev().find("img").attr("src");
-
     $(card).addClass("card_has_been_selected");
-
+    $(card).hide();
     if (first_card_clicked == null) {
         console.log("first card clicked");
         first_card_clicked = img_src;
         first_card_reference = card;
-        card_animation(card);
+        //card_animation(card);
     } else {
         second_card_clicked = img_src;
         second_card_reference = card;
-        card_animation(card);
+        //card_animation(card);
         console.log("second card clicked!");
         attempts += 1;
         display_stats();
@@ -76,7 +75,7 @@ function card_clicked(card) {
             match_counter++;
             console.log(match_counter);
             $("#matches .value").text(match_counter);
-            $(".card_has_been_selected").toggleClass('match');
+            $(card).addClass('match');
             console.log('a match!');
             first_card_clicked = null;
             second_card_clicked = null;
@@ -84,7 +83,9 @@ function card_clicked(card) {
             if (match_counter === total_possible_matches) {
                 alert("You've Won!");
                 games_played++;
-                //$(".card_has_been_selected").show();
+                $(".card_has_been_selected").show();
+                first_card_clicked = null;
+                second_card_clicked = null;
             }
         } else {
             console.log("card", card);
@@ -93,26 +94,27 @@ function card_clicked(card) {
             console.log("they don't match");
             waittime = setTimeout(function () {
                 console.log("set timeout");
-                card_animation(first_card_reference);
-                card_animation(second_card_reference);
+                //card_animation(first_card_reference);
+                //card_animation(second_card_reference);
+                $(first_card_reference).show();
+                $(second_card_reference).show();
             }, 2000);
         }
         howAccurate();
     }
 }
 
-var card_animation = function (card) {
-    if(typeof card == 'undefined'){
-        $('.animation-back').removeClass('animation-back');
-        return;
-    }
-    $(card).toggleClass("animation-back");
-    var find_card_front = $(card).parent().find(".card_front");
-    find_card_front.toggleClass("animation-front");
-    //want to see what card actually is in this function.
-    console.log('card is ', card);
-};
-
+//var card_animation = function (card) {
+//    if(typeof card == 'undefined'){
+//        $('.animation-back').removeClass('animation-back');
+//        return;
+//    }
+//    $(card).toggleClass("animation-back");
+//    var find_card_front = $(card).parent().find(".card_front");
+//    find_card_front.toggleClass("animation-front");
+//    //want to see what card actually is in this function.
+//    console.log('card is ', card);
+//};
 
 function howAccurate() {
     accurate = match_counter / attempts;
@@ -123,18 +125,20 @@ function howAccurate() {
         $("#accuracy").find(".value").text(accurate_percent + "%");
     }
 }
+
 function reset_stats() {
     accurate = 0;
     match_counter = 0;
     attempts = 0;
 }
+
 function display_stats() {
     $("#games_played .value").text(games_played);
     $("#matches .value").text(match_counter);
     $("#attempts .value").text(attempts);
     $("#accuracy .value").text(accurate);
-
 }
+
 function reset_clicked(reset) {
     $("#games_played .value").text(++games_played);
     console.log("games played add 1");
